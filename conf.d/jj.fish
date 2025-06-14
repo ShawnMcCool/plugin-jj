47ae370502520ac0e2337cba0e607b5833178cef
Mudit Kapoor <kaps.mudit@gmail.com>
2025-06-14 13:25:10 +0530

    add attribution

--- conf.d/jj.fish
+++ conf.d/jj.fish
@@ -1,3 +1,4 @@
+# Taken from jhillyerd/plugin-git
 # fisher initialization, protected as omf also tries to run it.
 set -q fisher_path; or set -l fisher_path $__fish_config_dir
 if test -f $fisher_path/functions/__jj.init.fish
--- init.fish
+++ init.fish
@@ -1,7 +1,8 @@
+# Taken from jhillyerd/plugin-git
 # omf initialization.
 # $path is only defined for oh-my-fish. home-manager activates this plugin by
 # adding the full path of functions/ to fish_function_path, and then sourcing
-# init.fish, so let's skip sourcing __git.init.fish before calling __git.init.
+# init.fish, so let's skip sourcing __jj.init.fish before calling __jj.init.
 set -l __jj_init_path "$path/functions/__jj.init.fish"
 if [ -f "$__jj_init_path" ];
     source "$__jj_init_path"
